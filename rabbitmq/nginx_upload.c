c1d72001b436e5cb63ddf0d4886fdb758b58b32d
yoyao <fwxshiwo@gmail.com>
2018-03-27 22:42:21 +0800

    nginx upload module

---
+++ rabbitmq/nginx_upload.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fastcgi.h>
+#include <fcgi_stdio.h>
+
+#include <fastdfs/fdfs_client.h>
+
+
+int main(int argc,char *argv[])
+{
+    int count=0;
+    while (FCGI_Accept() >= 0)
+    {
+        printf("Content-type: text/html\r\n"
+               "\r\n"
+               ""
+               "FastCGI Hello!"
+               "Request number %d running on host:%s "
+               "Request Method:%s "
+               "Process ID: %d\n", ++count, getenv("SERVER_NAME"), getenv("REQUEST_METHOD"), getpid());
+    }
+    return 0;
+}
+
