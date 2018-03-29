085bd05b269544d9c0699996462c90f32b1d8f96
yoyao <fwxshiwo@gmail.com>
2018-03-29 10:42:40 +0800

    fcgi exmaple

--- rabbitmq/main_upload.cpp
+++ rabbitmq/main_upload.cpp
@@ -16,12 +16,13 @@
 #include <event2/http.h>
 #include <event2/http_struct.h>
 #include <iostream>
-#include "include/cJSON.h"
+#include <json/json.h>
 
 #define LISTEN_PORT 31763
 #define READ_BUFF_SIZE (1024*4)
 
 using namespace std;
+using namespace Json;
 
 /*
  * typedef void (*evconnlistener_cb)(struct evconnlistener *, evutil_socket_t, struct sockaddr *, int socklen, void *);
@@ -39,7 +40,7 @@ void OnClientConnect(struct evconnlistener *listener, evutil_socket_t fd, struct
 void OnRequest(struct evhttp_request *request,void *arg);
 int WorkSaveFile(struct bufferevent *bufev,int fd);
 
-int main(int argc, char *argv[])
+int main_http(int argc, char *argv[])
 {
     int lsfd,ret=0;
 
@@ -165,221 +166,20 @@ void OnSockReadable(struct bufferevent *bufev,void *arg)
     char jsonlen_buf[4]={0};
     char *p_json=NULL;
 
+return ;
     TranInfo *p_tran=(TranInfo*)arg;
-
-    while (1)
-    {
-        if(p_tran->fd<0)
-        {
-            len=bufferevent_read(bufev,(char*)&jsonlen,4);
-            if(len==0)
-            {
-                ret=-1;
-                //缓冲区没有内容但是强行读取
-                break;
-            }
-
-            //printf("datalen %d\n",jsonlen);
-            if(jsonlen>210000)
-            {
-                printf("data too large\n");
-                ret=-1;
-                break;
-            }
-            p_json=(char*)calloc(sizeof(char),jsonlen+1);
-            len=bufferevent_read(bufev,p_json,jsonlen);
-            if(len==0 && errno==EAGAIN)
-            {
-                ret=-1;
-                break;
-            }
-            printf("json string :\n%s\n",p_json);
-            cJSON *jdoc= cJSON_Parse(p_json);
-            if(jdoc ==NULL)
-            {
-                ret=1;
-                break;
-            }
-            cJSON *cj_name=NULL;
-            cj_name=cJSON_GetObjectItem(jdoc,"localname");
-            if(cj_name==NULL)
-            {
-                ret=7;
-                break;
-            }
-
-            cJSON *cj_size=NULL;
-            cj_size=cJSON_GetObjectItem(jdoc,"datasize");
-            if(cj_size==NULL)
-            {
-                ret=2;
-                break;
-            }
-            cJSON *cj_filehash=NULL;
-            cj_filehash =cJSON_GetObjectItem(jdoc,"filehash");
-            if(cj_filehash==NULL)
-            {
-                ret=3;
-                break;
-            }
-            cJSON *cj_user=cJSON_GetObjectItem(jdoc,"user");
-            cJSON *cj_token=NULL;
-            cj_token =cJSON_GetObjectItem(cj_user,"token");
-            if(cj_token==NULL)
-            {
-                ret=4;
-                break;
-            }
-            cJSON *cj_uid=NULL;
-            cj_uid =cJSON_GetObjectItem(cj_user,"uid");
-            if(cj_uid==NULL)
-            {
-                ret=5;
-                break;
-            }
-
-            p_tran->datasize=cj_size->valueint;
-
-            char path[1024]={0};
-            sprintf(path,"/home/fgo/document/course/cloud-disk/common/%s",cj_name->valuestring);
-
-            p_tran->fd= open(path,O_CREAT|O_WRONLY|O_APPEND,0644);
-            if(p_tran->fd<0)
-            {
-                ret=6;
-                break;
-            }
-            free(p_json);
-        }
-        else
-        {
-            len=bufferevent_read(bufev,buff,READ_BUFF_SIZE);
-            //printf("read length %d\n",len);
-            if(len==0)
-            {
-                ret=-1;
-                break;
-            }
-            int wlen= write(p_tran->fd,buff,len);
-            if(wlen !=len)
-            {
-                ret=-2;
-                break;
-            }
-            p_tran->readsize+=wlen;
-            char result[512]={0};
-            sprintf(result,"{\"status\":%d,\"error\":%d,\"succ\":%d,\"msg\":\"succ%d\"}",ret,ret,ret,ret);
-            if(p_tran->readsize==p_tran->datasize)
-            {
-                bufferevent_write(bufev,result,strlen(result));
-                printf("The part end\n");
-                close(p_tran->fd);
-                p_tran->fd=-1;
-                p_tran->readsize=0;
-                p_tran->datasize=0;
-            }
-              memset(buff,0,len);
-        }
-    }
-
 
 }
 
 int WorkSaveFile(struct bufferevent *bufev,int fd)
 {
-    char *p_content;
+    char *p_content=NULL;
     char *p_back=NULL;
     char *p_json=NULL;
     int len;
     int datalen=-1;
     int ret=0;
-    //  int fd;
-
-    do
-    {
-        char *tmp=NULL;
-        strstr(p_content,"}\r\n\r\n");
-        //+2是为了把大括号和\0复制进去
-        p_json=(char*)calloc(sizeof(char),abs(tmp-p_content)+2);
-
-        //+1是为了把大括号复制进去
-        memcpy(p_json,p_content,abs(tmp-p_content)+1);
-        printf("pjson %s\n",p_json);
-        cJSON *jdoc= cJSON_Parse(p_json);
-        if(jdoc ==NULL)
-        {
-            ret=1;
-            break;
-        }
-        cJSON *cj_name=NULL;
-        cj_name=cJSON_GetObjectItem(jdoc,"localname");
-        if(cj_name==NULL)
-        {
-            ret=7;
-            break;
-        }
-
-        cJSON *cj_size=NULL;
-        cj_size=cJSON_GetObjectItem(jdoc,"datasize");
-        if(cj_size==NULL)
-        {
-            ret=2;
-            break;
-        }
-        cJSON *cj_filehash=NULL;
-        cj_filehash =cJSON_GetObjectItem(jdoc,"filehash");
-        if(cj_filehash==NULL)
-        {
-            ret=3;
-            break;
-        }
-        cJSON *cj_user=cJSON_GetObjectItem(jdoc,"user");
-        cJSON *cj_token=NULL;
-        cj_token =cJSON_GetObjectItem(cj_user,"token");
-        if(cj_token==NULL)
-        {
-            ret=4;
-            break;
-        }
-        cJSON *cj_uid=NULL;
-        cj_uid =cJSON_GetObjectItem(cj_user,"uid");
-        if(cj_uid==NULL)
-        {
-            ret=5;
-            break;
-        }
-
-        char path[1024]={0};
-        sprintf(path,"/home/fgo/document/course/cloud-disk/common/%s",cj_name->valuestring);
-
-        fd= open(path,O_CREAT|O_WRONLY|O_APPEND,0644);
-        if(fd<0)
-        {
-            ret=6;
-            break;
-        }
-        //  *pfd=fd;
-        int wlen= write(fd,tmp+5,cj_size->valueint);
-        if(wlen !=cj_size->valueint)
-        {
-            ret=-2;
-            break;
-        }
-        cJSON *cj_isend=NULL;
-        cj_isend=cJSON_GetObjectItem(jdoc,"isend");
-        if(cj_isend !=NULL)
-        {
-            //上传到fdfs
-
-        }
-
-    }while(0);
-
-    if(p_json!=NULL)
-    {
-        free(p_json);
-        p_json=NULL;
-    }
+//先搞到数据 然后上传到fdfs
     return ret;
 }
 
@@ -394,7 +194,5 @@ void OnsocketEvent(struct bufferevent *bufev, short events, void *arg)
             free(p_tran);
         }
         bufferevent_free(bufev);
-
-
     }
 }
